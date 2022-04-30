acd9cb9e7ce66ba530297de2b83e9656c8dbd66e
Kibria934 <kibriahossain725@gmai.com>
2022-05-01 03:08:30 +0600

    trying to add update api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //midlleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rozc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,26 +16,39 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 
 async function run() {
   try {
-      await client.connect();
-      const fruitsCollection =client.db('fruitShop').collection('fruits');
-      
-app.get('/fruits',async(req,res)=>{
-    const query = {};
-      const cursor=fruitsCollection.find(query);
-      const result = await cursor.toArray()
-      res.send(result)
-})
-app.get('/fruit/:id',async(req,res)=>{
-  const id = req.params.id;
-  const query = {_id:ObjectId(id)};
-  const result = await fruitsCollection.findOne(query);
-  res.send(result);
+    await client.connect();
+    const fruitsCollection = client.db("fruitShop").collection("fruits");
 
-})
+    app.get("/fruits", async (req, res) => {
+      const query = {};
+      const cursor = fruitsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/fruit/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruitsCollection.findOne(query);
+      res.send(result);
+     
+     console.log(uri);
+      app.put("/fruit/:id", async (req, res) => {
+        const id = req.params.id;
+        const updateFruit = req.body;
+        // console.log(updateFruit);
+        console.log(updateFruit);
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        updateDoc = {
+          $set: updateFruit,
+        };
+        const result = await fruitsCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+      });
+    });
   } finally {
   }
 }
