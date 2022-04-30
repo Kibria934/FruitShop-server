57b779956dc3a4ebd0ffd3c71cc19f66f1f3c974
Kibria934 <kibriahossain725@gmai.com>
2022-04-30 17:28:47 +0600

    frut id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -29,7 +29,13 @@ app.get('/fruits',async(req,res)=>{
       const result = await cursor.toArray()
       res.send(result)
 })
+app.get('/fruit/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:ObjectId(id)};
+  const result = await fruitsCollection.findOne(query);
+  res.send(result);
 
+})
   } finally {
   }
 }
