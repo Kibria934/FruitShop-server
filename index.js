bc852c2859d28da2a94749f657f73e173959b74e
Kibria934 <kibriahossain725@gmai.com>
2022-05-31 09:29:43 +0600

    testing

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const verifyJwt = (req, res, next) => {
   })
   next()
 };
-
+// hello
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rozc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
